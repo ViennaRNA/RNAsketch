de0db262ae2f06c5e745f737c9909f585078ae3c
Stefan Hammer <s.hammer@univie.ac.at>
2015-10-27 12:59:25 +0100

    made tristable example multistable

--- designs/hoehner-multistable-example.py
+++ designs/hoehner-multistable-example.py
@@ -2,6 +2,7 @@ import RNAdesign as rd
 import RNA
 import argparse
 import sys
+import re
 
 # a tri-stable example target. (optional comment)
 # ((((....))))....((((....))))........
@@ -15,26 +16,40 @@ import sys
 def main():
     parser = argparse.ArgumentParser(description='Design a tri-stable example same to Hoehner 2013 paper.')
     parser.add_argument("-n", "--number", type=int, default=4, help='Number of designs to generate')
+    parser.add_argument("-p", "--progress", default=False, action='store_true', help='Show progress of optimization')
+    parser.add_argument("-i", "--input", default=False, action='store_true', help='Read custom structures and sequence constraints from stdin')
     args = parser.parse_args()
 
     # define structures
-    structures = ['((((....))))....((((....))))........',
-                  '........((((....((((....))))....))))',
-                  '((((((((....))))((((....))))....))))']
+    structures = []
+    constraint = ""
+    if (args.input):
+        for line in sys.stdin:
+            if re.match(re.compile("[\(\)\.]"), line, flags=0):
+                structures.append(line.rstrip('\n'))
+            elif re.match(re.compile("[ACGTUWSMKRYBDHVN]"), line, flags=0):
+                constraint = line.rstrip('\n')
+            elif re.search(re.compile("@"), line, flags=0):
+                break;
+    else:
+        structures = ['((((....))))....((((....))))........',
+            '........((((....((((....))))....))))',
+            '((((((((....))))((((....))))....))))']
+        constraint = 'NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN'
     
     # construct dependency graph with these structures
-    dg = rd.DependencyGraphMT(structures)
+    dg = rd.DependencyGraphMT(structures, constraint)
     
-    print "\n".join(structures) + "\n"
+    print("\n".join(structures) + "\n" + constraint + "\n")
     # print the amount of solutions
-    print 'Maximal number of solutions: ' + str(dg.number_of_sequences())
+    print('Maximal number of solutions: ' + str(dg.number_of_sequences()))
     # print the amount of connected components
     number_of_components = dg.number_of_connected_components()
-    print 'Number of Connected Components: ' + str(number_of_components)
+    print('Number of Connected Components: ' + str(number_of_components))
 
     for i in range(0, number_of_components):
-        print '[' + str(i) + ']' + str(dg.component_vertices(i))
-    print ''
+        print('[' + str(i) + ']' + str(dg.component_vertices(i)))
+    print('')
 
     # resulting sequence
     result_seq = ''
@@ -52,8 +67,9 @@ def main():
         # mutate globally for 1000 times and print
         for i in range(0, 500000):
             # write progress
-            sys.stdout.write("\rMutate global: {0:7.0f}/{1:5.0f} from NOS: {2:7.0f}".format(i, count, dg.mutate_global()))
-            sys.stdout.flush()
+            if (args.progress):
+                sys.stdout.write("\rMutate global: {0:7.0f}/{1:5.0f} from NOS: {2:7.0f}".format(i, count, dg.mutate_global()))
+                sys.stdout.flush()
             
             this_score = calculate_objective(dg.get_sequence(), structures);
             #print dg.get_sequence() + '\t' + str(this_score)
@@ -73,25 +89,31 @@ def main():
 
 # objective function: eos(1)+eos(2)+eos(3) - 3 * gibbs + 1 * ((eos(1)-eos(2))^2 + (eos(1)-eos(3))^2 + (eos(2)-eos(3))^2)
 def calculate_objective(sequence, structures):
-    eos = [0]
+    eos = []
     for struct in structures:
         eos.append(RNA.energy_of_struct(sequence, struct))
+    
     gibbs = RNA.pf_fold(sequence)
     
-    return eos[1]+eos[2]+eos[3] - 3 * gibbs[1] + 1 * (pow(eos[1]-eos[2], 2) + pow(eos[1]-eos[3], 2) + pow(eos[2]-eos[3], 2))
+    objective_difference_part = 0
+    for i, value in enumerate(eos):
+        for j in eos[i+1:]:
+            objective_difference_part += pow(value - j, 2)
+    
+    return sum(eos) - len(eos) * gibbs[1] + 1 * objective_difference_part
 
 # function which prints the result nicely to the screen
 def print_result(sequence, structures, score):
     sys.stdout.write("\r                                                       \r")
     sys.stdout.flush()
-    print sequence + '\t{0:4.5f}'.format(score)
+    print(sequence + '\t{0:4.5f}'.format(score))
     (mfe_struct, mfe_energy) = RNA.fold(sequence)
     
     for struct in structures:
         eos = RNA.energy_of_struct(sequence, struct)
-        print struct + '\t{0:4.5f}\t{1:4.5f}'.format(eos, mfe_energy-eos)
+        print(struct + '\t{0:4.5f}\t{1:4.5f}'.format(eos, mfe_energy-eos))
     
-    print mfe_struct + '\t{0:4.5f}'.format(mfe_energy)
+    print(mfe_struct + '\t{0:4.5f}'.format(mfe_energy))
 
 
 if __name__ == "__main__":
