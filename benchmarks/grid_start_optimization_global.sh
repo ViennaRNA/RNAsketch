d295bc1197dde807e1f90c3857d8f8cce0c92ee4
Stefan Hammer <s.hammer@univie.ac.at>
2015-10-28 13:39:06 +0100

    bug in grid jobs

--- benchmarks/grid_start_optimization_global.sh
+++ benchmarks/grid_start_optimization_global.sh
@@ -2,14 +2,14 @@
 
 #$ -N global_optimization_duration
 #$ -q c_main.q
-#$ -e /scr/minos/jango/Software/RNAdesign-toolbox/benchmarks/optimization_duration_output
+#$ -e /scr/minos/jango/Software/RNAdesign-toolbox/benchmarks/optimization_duration_output/error_global.log
 #$ -o /scr/minos/jango/Software/RNAdesign-toolbox/benchmarks/optimization_duration_output
-#$ -t 1-500
+#$ -t 1-40
 
 #################################### DONT FORGET TO CHANGE ALL FOLDERS AND SETTINGS!!
 
 ###$SGE_TASK_ID
-DPATH=/scr/minos/jango/Software/RNAdesign-toolbox/benchmarks/optimization_duration_output/error_global.log
+DPATH=/scr/minos/jango/Software/RNAdesign-toolbox/benchmarks/optimization_duration_output
 SCRIPT=/scr/minos/jango/Software/RNAdesign-toolbox/benchmarks/optimization_duration.py
 START=$(($SGE_TASK_ID*1000-1000))
 END=$(($SGE_TASK_ID*1000-1))
--- benchmarks/grid_start_optimization_local.sh
+++ benchmarks/grid_start_optimization_local.sh
@@ -2,14 +2,14 @@
 
 #$ -N local_optimization_duration
 #$ -q c_main.q
-#$ -e /scr/minos/jango/Software/RNAdesign-toolbox/benchmarks/optimization_duration_output
+#$ -e /scr/minos/jango/Software/RNAdesign-toolbox/benchmarks/optimization_duration_output/error_local.log
 #$ -o /scr/minos/jango/Software/RNAdesign-toolbox/benchmarks/optimization_duration_output
-#$ -t 1-500
+#$ -t 1-40
 
 #################################### DONT FORGET TO CHANGE ALL FOLDERS AND SETTINGS!!
 
 ###$SGE_TASK_ID
-DPATH=/scr/minos/jango/Software/RNAdesign-toolbox/benchmarks/optimization_duration_output/error_local.log
+DPATH=/scr/minos/jango/Software/RNAdesign-toolbox/benchmarks/optimization_duration_output
 SCRIPT=/scr/minos/jango/Software/RNAdesign-toolbox/benchmarks/optimization_duration.py
 START=$(($SGE_TASK_ID*1000-1000))
 END=$(($SGE_TASK_ID*1000-1))
