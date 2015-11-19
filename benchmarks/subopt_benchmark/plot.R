45e1ad3156e84b2932b8971644f3ed55ef70d290
Stefan Hammer <s.hammer@univie.ac.at>
2015-11-19 16:57:25 +0100

    even more plots

--- benchmarks/subopt_benchmark/plot.R
+++ benchmarks/subopt_benchmark/plot.R
@@ -27,17 +27,48 @@ q(status=1);
 names<-cbind("length", "structures", "graph_construction", "num_cc", "max_special_ratio", "mean_special_ratio", "nos", "construction_time", "sample_time");
 data <- read.csv(opt$file, header=FALSE, sep = ";", dec = ".", comment.char='#', col.names=names);
 
+
+
+data_failed <- subset(data, graph_construction==0)
+data_failed <- data_failed[, (names(data_failed) %in% c("length", "structures"))]
+# plot failed
+pdf(paste(opt$file, "_failed.pdf", sep=""))
+p <- qplot(data=data_failed, x=structures, y=length, geom='bin2d')
+p + xlab("Number of Structural Constraints") +
+    ylab("Design Length") + 
+    ggtitle("Graph Construction Timeouts")
+dev.off()
+
 times <- c(grep("time$", names(data), value=TRUE))
 data_sub <- subset(data, graph_construction!=0)
 data_melt <- melt(data_sub, id.vars="mean_special_ratio", measure.vars=times)
 
-number_ticks <- function(n) {function(limits) pretty(limits, n)}
+cor.mtest <- function(mat, conf.level = 0.95) {
+    mat <- as.matrix(mat)
+    n <- ncol(mat)
+    p.mat <- lowCI.mat <- uppCI.mat <- matrix(NA, n, n)
+    diag(p.mat) <- 0
+    diag(lowCI.mat) <- diag(uppCI.mat) <- 1
+    for (i in 1:(n - 1)) {
+        for (j in (i + 1):n) {
+            tmp <- cor.test(mat[, i], mat[, j], conf.level = conf.level)
+            p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+            lowCI.mat[i, j] <- lowCI.mat[j, i] <- tmp$conf.int[1]
+            uppCI.mat[i, j] <- uppCI.mat[j, i] <- tmp$conf.int[2]
+        }
+    }
+    return(list(p.mat, lowCI.mat, uppCI.mat))
+}
 
 # plot correlation plot
 pdf(paste(opt$file, "_corrplot.pdf", sep=""))
 cc <- cor(data_sub[, !names(data_sub)=="graph_construction"])
-corrplot(cc, method = "color")
+res1 <- cor.mtest(cc, 0.95)
+corrplot(cc, method = "color", order = "hclust", diag= FALSE, p.mat = res1[[1]], insig = "p-value")
 dev.off()
+
+
+number_ticks <- function(n) {function(limits) pretty(limits, n)}
 
 # plot times vs special ratio
 pdf(paste(opt$file, "_time.pdf", sep=""))
@@ -50,6 +81,5 @@ p + geom_point() +
     xlab("mean Ratio Specials/Vertices") +
     ylab("Time [s]") + 
     ggtitle("Time vs Complexity")
-
 dev.off()
 # plot done!
