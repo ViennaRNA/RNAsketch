64f0da736040c6b1c65f799ebc2ed397a487136d
TBI Bioinf Account <bioinf@tbi.univie.ac.at>
2015-11-23 12:09:55 +0100

    generates latex result table

--- benchmarks/MODENA/modena_result.R
+++ benchmarks/MODENA/modena_result.R
@@ -1,79 +1,59 @@
 # Packages
 check_package <- function(x) {
   if (!require(x,character.only = TRUE)) {
-      install.packages(x,dep=TRUE)
+    install.packages(x,dep=TRUE)
   }
 }
 
-check_package("ggplot2")
-# check_package("getopt")
-check_package("plyr")
-check_package("xtable")
+check_package("Hmisc")
 
-infiles <- dir(pattern='\\.out$')
+infiles <- list.files(pattern = '*.out')
 
-change.files <- function(file){
-  data <- read.csv(file, header=TRUE, sep=";", dec = ".", comment.char='#')
+evaluate <- function(file) {
+  data <- read.csv(file, header = TRUE, sep=";", dec = ".", comment.char='#')
+  data <- cbind(data,path=file) # add column with filename
   
+  sequence_length <- data[1,3] #sequence length: first entry of column 3, since all entries are the same
+  sum_n1 <- sum(data$n1)
+  sum_n2 <- sum(data$n2)
+  
+  # find min d2 and min d1, if there are more than one min d2
+  min_d2 <-  data[data[,7] == min(data[,7]),] # rows where column 7(= d2) is minimum
+  min_d1 <- min(min_d2[,6]) # since min_d2 contains all entries where d2 is min, it is sufficient to find one minimum (does not matter if there are more than one)
+
+  d1_min <- min_d1 # not necessary
+  d2_min <- min_d2[1,7] # all entries in this column mimima, therefore take first entry
+ 
+  # mean, median, standard deviation and standard error of d1 and d2, not used 
+  #   mean_d1 <- mean(data$d1)
+  #   median_d1 <- median(data$d1)
+  #   sd_d1 <- sd(data$d1)
+  #   se_d1 <- sd_d1/sqrt(length(data$d1))
+  #   mean_d2 <- mean(data$d2)
+  #   median_d2 <- median(data$d2)
+  #   sd_d2 <- sd(data$d2)
+  #   se_d2 <- sd_d2/sqrt(length(data$d2))
+  
+  # result values
+  result <-data.frame(file, sequence_length, d1_min, d2_min, sum_n1, sum_n2)
+ 
+  return(result)
 }
 
+all_infiles <- lapply(infiles, evaluate)
+all_infiles <- do.call(rbind,all_infiles)
 
-lapply(infiles , change.files)
+colnames(all_infiles) <- c("RNA", "l", "d1", "d2", "n1", "n2") # RNA = file
 
+# calculate mean and median (without SV11!!!) from allinfiles d1 and d2  
+all_without_SV11 <- all_infiles[all_infiles[,1] != "modena_comp_rb5_edif0.0.inp.out",] # richtiges file ausnehmen (SV11) 
+mean_all_d1 <- mean(all_without_SV11$d1)
+median_all_d1 <- median(all_without_SV11$d1)
+mean_all_d2 <- mean(all_without_SV11$d2)
+median_all_d2 <- median(all_without_SV11$d2)
 
+all_infiles <- rbind(all_infiles, data.frame(RNA="mean", l = "-", d1= mean_all_d1, d2 = mean_all_d2, n1 = "-", n2= "-"))
+all_infiles <- rbind(all_infiles, data.frame(RNA="median", l = "-", d1= median_all_d1, d2 = median_all_d2, n1 = "-" , n2= "-"))
 
-n1 <- data$n1
-n2 <- data$n2
-
-d1 <- data$d1
-d2 <- data$d2
-
-length_of_sequence <- mean(data$length.of.sequence)
-
-## length of sequence weil gleich, besser designname oder so...
-cdata1 <- ddply(data, c("length.of.sequence"), summarise,
-               struct = '1',
-               sum_n1 = sum(n1),
-               sum_n2 = sum(n2),
-                       
-               mean_d1 = mean(d1),
-               median_d1 = median(d1),
-               sd_d1 = sd(d1),
-               se_d1 = sd_d1/sqrt(length(d1)),
-    
-               mean_d2 = mean(d2),
-               median_d2 = median(d2),
-               sd_d2 = sd(d2),
-               se_d2 = sd_d2/sqrt(length(d2))
-
-)
-
-
-sum_n1 <- sum(n1)
-sum_n2 <- sum(n2)
-
-# find min d2 und d1
-mins_d2 <- which( d2 == min(d2)) #mins_d2 vector mit Positionen wo min values
-if ((length(mins_d2) >= 2)){
-    # smallest_d1 <- 135
-    for (i in 1: length(mins_d2)){
-        current <- d1[mins_d2[i]]
-        if (current < smallest_d1){
-            smallest_d1 <- current 
-            index <- mins_d2[i]
-        }
-    }
-    d1 <- d1[index]
-    d2 <- d2[index]
-}
-
-result <- c(length_of_sequence,d1, d2, sum_n1, sum_n2)
-print(result)
-
-   
-    
-
-
-
-
-
+# generate latex table
+latex(all_infiles, cdec=c(0,0,2,2,0,0), na.blank=TRUE, booktabs=FALSE, table.env=FALSE, center="none", file="", title="")
