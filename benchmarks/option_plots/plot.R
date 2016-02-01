9e6d1d7e6fa7e601d55a621d62799d1e42526c47
Stefan Hammer <s.hammer@univie.ac.at>
2016-02-01 16:46:36 +0100

    update Rscript plotting and rename hoener example

--- benchmarks/option_plots/plot.R
+++ benchmarks/option_plots/plot.R
@@ -13,7 +13,9 @@ check_package("plyr")
 spec = matrix(c(
     'file','f', 1, "character",
     'x','x', 1, "character",
-    'label','l', 1, "character",
+    'xlabel','l', 1, "character",
+    'y','y', 1, "character",
+    'ylabel','p', 1, "character",
     'title','t', 1, "character"
 ), byrow=TRUE, ncol=4);
 opt = getopt(spec);
@@ -23,13 +25,19 @@ if ( is.null(opt$file)) {
 cat(getopt(spec, usage=TRUE));
 q(status=1);
 }
-
-if ( is.null(opt$label)) {
-opt$label = 'x';
+if ( is.null(opt$xlabel)) {
+opt$xlabel = 'x';
+}
+if ( is.null(opt$y)) {
+opt$y = 'diff_eos_mfe';
+}
+if ( is.null(opt$ylabel)) {
+opt$ylabel = expression(paste("mean ", delta, "E [kcal]"));
 }
 if ( is.null(opt$title)) {
 opt$title = '';
 }
+
 # Script
 # jump;exit;mode;score;num_mutations;seq_length;sequence;graph_construction;num_cc;max_specials;max_component_vertices;
 # max_special_ratio;mean_special_ratio;nos;construction_time;sample_time;
@@ -47,7 +55,7 @@ cdata <- data.frame()
 for (i in 1:number_structures) {
     colNameMFE<-paste("mfe_reached", toString(i-1), sep="_")
     print(colNameMFE)
-    colNameDiff<- paste("diff_eos_mfe", toString(i-1), sep="_")
+    colNameDiff<- paste(opt$y, toString(i-1), sep="_")
     colNameProb<- paste("prob", toString(i-1), sep="_")
     
     eval(parse(text = paste0(
@@ -66,13 +74,18 @@ cdata <- ddply(cdata, c("x"),transform,ystart = cumsum(dEmean),yend = cumsum(dEm
 head(cdata)
 number_ticks <- function(n) {function(limits) pretty(limits, n)}
 
+miny=5.5
+if ( opt$y == 'prob') {
+miny = 1;
+}
+
 # Standard deviation of the mean as error bar
-pdf(paste(opt$file, ".pdf", sep=""))
+pdf(paste(opt$y, ".", opt$file, ".pdf", sep=""))
 p <- ggplot(cdata, aes(x=x, y=dEmean, fill=struct)) + geom_bar(stat="identity") +
     scale_y_continuous(limits = c(-0.2, NA), breaks=number_ticks(5)) +
-    expand_limits(y=5.5) +
-    ylab(expression(paste("mean ", delta, "E [kcal]"))) +
-    xlab(opt$label) +
+    expand_limits(y=miny) +
+    ylab(opt$ylabel) +
+    xlab(opt$xlabel) +
     labs(fill="Target Structure") +
     ggtitle(opt$title)
 
@@ -83,45 +96,3 @@ p + geom_segment(aes(xend=x,y=ystart,yend=yend), size = 0.1) +
     #geom_point(aes(x=x,y=-0.2, colour=pmean), shape = 15, show_guide = FALSE, size = 3)
 dev.off()
 # plot done!
-if (FALSE) {
-    all_diff_eos_mfe <- data[,grep("^diff_eos_mfe_", colnames(data))]
-    sort_diff_eos_mfe <- data.frame(t(apply(all_diff_eos_mfe, 1, sort, decreasing = FALSE)))
-    colnames(sort_diff_eos_mfe) <- cbind(colnames(all_diff_eos_mfe))
-    sort_diff_eos_mfe$x <- data$x
-    sdata <- data.frame()
-    head(sort_diff_eos_mfe)
-
-    cdata <- data.frame()
-    for (i in 1:number_structures) {
-        colNameDiff<- paste("diff_eos_mfe", toString(i-1), sep="_")
-        
-        eval(parse(text = paste0(
-        "currentcdata <- ddply(sort_diff_eos_mfe, c('x'),\
-        summarise, struct = \"", toString(i), "\", dEmean = mean(", colNameDiff, "),\
-        dEsd   = sd(", colNameDiff, "), dEse   = dEsd / sqrt(length(", colNameDiff, ")))")))
-        
-        #currentcdata
-        cdata <- rbind(cdata, currentcdata)
-    }
-
-    # add the coordinates for the error bars
-    cdata <- ddply(cdata, c("x"),transform,ystart = cumsum(dEmean),yend = cumsum(dEmean) + dEsd)
-    head(cdata)
-
-    # Standard deviation of the mean as error bar
-    pdf(paste(opt$file, "_sorted.pdf", sep=""))
-    p <- ggplot(cdata, aes(x=x, y=dEmean, fill=struct)) + geom_bar(stat="identity") +
-        scale_y_continuous(limits = c(-0.2, NA), breaks=number_ticks(5)) +
-        expand_limits(y=5.5) +
-        ylab(expression(paste("mean ", delta, "E [kcal]"))) +
-        xlab(opt$label) +
-        labs(fill="Target Structure") +
-        ggtitle(opt$title)
-
-    p + geom_segment(aes(xend=x,y=ystart,yend=yend), size = 0.1) +
-        geom_point(aes(x=x,y=yend), shape = "-", show_guide = FALSE, size = 2)
-    dev.off()
-    # plot done!
-}
-
-
