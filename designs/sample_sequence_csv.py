dd8f930e525c8b67ed7f13b4a57cd2f56dd6fa1b
Birgit Tschiatschek <birgit.tschiatschek@univie.ac.at>
2016-04-12 18:18:01 +0200

    option for multiple start_sequences

--- designs/sample_sequence_csv.py
+++ designs/sample_sequence_csv.py
@@ -91,77 +91,115 @@ def plot_sequence_objective(args):
         # remember general DG values
         graph_properties = get_graph_properties(dg)
 
-        csv_file = open(args.out_file + ".csv", 'w') #TODO bessren standaradnamen
-
-        # create a initial design object
-        if (args.nupack):
-            design = nupackDesign(structures, start_sequence)
-        else:
-            design = vrnaDesign(structures, start_sequence)
-
-        if not design.sequence:
-            dg.sample()
-            design.sequence = dg.get_sequence()
-        else:
-            dg.set_sequence(design.sequence)
-   
-        # print header for csv file
-        csv_file.write(";".join(["objective 1",
-                        "objective 2",
-                        "score",
-                        "sequence \n"]))
-
-        # calculate objectives for initial sequence
-        x_center = calculate_objective_1(design)
-        y_center = calculate_objective_2(design)
-    
-        # print csv input for initial sequence
-        score = x_center + args.weight * y_center
-                   
-        # store entries -> just unique entries
-        samples = set()
-        samples.add(design.sequence)
+        start_sequences = [] 
         
-        csv_file.write(";".join([str(x_center),
-                str(y_center),
-                str(score),
-                design.sequence]) + "\n")
+        if args.start_seq is not None:
+            #start_sequences = []
+            #read_startsequences(args.start_seq)
+            #print(start_sequences)
+            
+            with open(args.start_seq) as f:
+                data = f.read()
+            start_seq = data.split("\n")  
+            for seq in start_seq:
+                if seq != '':
+                    start_sequences.append(seq.rstrip('\n'))
+       
+        if args.start_seq is None:
+            start_sequences.append(start_sequence)
+        
+        for i in range(0,len(start_sequences)):  
+        
+            if args.start_seq is not None:   
+                csv_file_name = args.out_file + "_start_seq_" + str(i + 1)   
+            else:
+                csv_file_name = args.out_file   
                 
-        for i in range(1, number):
-            
-            while design.sequence in samples:
-                (mut_nos, sample_count) = PyDesign._sample_sequence(dg, design, args.mode, args.sample_steps)
-                
-                dg.revert_sequence(sample_count)
-                        
-            x_new = calculate_objective_1(design)
-            y_new = calculate_objective_2(design)
-            
-            
-            if args.progress:
-                sys.stdout.write("\r# Sampling: {0:7.0f}/{1:5.0f}".format(i + 1, number) + " " * 20)
-                sys.stdout.flush()
+            csv_file = open(csv_file_name + ".csv", 'w') #TODO bessren standaradnamen
+            start_sequence = start_sequences[i]
 
-            score = x_new + args.weight * y_new
-          
-           
-            csv_file.write(";".join([str(x_new),
-                str(y_new),
-                str(score),
-                design.sequence]) + "\n")
-      
-            samples.add(design.sequence) 
-                    
+            # create an initial design object
+            if (args.nupack):
+                design = nupackDesign(structures, start_sequence)
+            else:
+                design = vrnaDesign(structures, start_sequence)
+
+            if not design.sequence:
+                dg.sample()
+                design.sequence = dg.get_sequence()
+            else:
+                dg.set_sequence(design.sequence)
+
+            # print header for csv file
+            csv_file.write(";".join(["objective 1",
+                            "objective 2",
+                            "score",
+                            "sequence \n"]))
+
+            # calculate objectives for initial sequence
+            x_center = calculate_objective_1(design)
+            y_center = calculate_objective_2(design)
+
+            # print csv input for initial sequence
+            score = x_center + args.weight * y_center
+
+            # store entries -> just unique entries
+            samples = set()
+            samples.add(design.sequence)
+
+            csv_file.write(";".join([str(x_center),
+                    str(y_center),
+                    str(score),
+                    design.sequence]) + "\n")
+
+            for i in range(1, number):
+
+                while design.sequence in samples:
+                    (mut_nos, sample_count) = PyDesign.sample_sequence(dg, design, args.mode, args.sample_steps)
+
+                    dg.revert_sequence(sample_count)
+
+                x_new = calculate_objective_1(design)
+                y_new = calculate_objective_2(design)
+
+
+                if args.progress:
+                    sys.stdout.write("\r# Sampling: {0:7.0f}/{1:5.0f}".format(i + 1, number) + " " * 20)
+                    sys.stdout.flush()
+
+                score = x_new + args.weight * y_new
+
+
+                csv_file.write(";".join([str(x_new),
+                    str(y_new),
+                    str(score),
+                    design.sequence]) + "\n")
+
+                samples.add(design.sequence)
                 
+            print('\n # Output file: %s'% csv_file_name + '.csv')
+
     else:
-        print('# Construction time out reached!')
+            print('# Construction time out reached!')
 
-    print('\n # Output file: %s'% args.out_file + '.csv')
+
+    
+def read_startsequences(filename):
+    start_sequences = []
+    with open(args.start_seq) as f:
+        data = f.read()
+        start_seq = data.split("\n")  
+        for seq in start_seq:
+            if seq != '':
+                start_sequences.append(seq.rstrip('\n'))
+   #print(start_sequences)
+    return start_sequences
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Plot...')
     parser.add_argument("-f", "--file", type = str, default=None, help='Read file in *.inp format')
     parser.add_argument("-i", "--input", default=False, action='store_true', help='Read custom structures and sequence constraints from stdin')
+    parser.add_argument("-y", "--start_seq", type = str, default=None, help='Read start_seq')
     parser.add_argument("-m", "--mode", type=str, default='sample_global', help='Mode for getting a new sequence: sample, sample_local, sample_global, sample_strelem')
     parser.add_argument("-s", "--sample_steps", type=int, default=1, help='Count how many times to do the sample operation')
     parser.add_argument("-n", "--number", type=int, default=1000, help='Define LNOS')
